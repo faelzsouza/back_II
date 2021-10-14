68b22f8d72143123280b0f0a8692e8cc957f6738
Rafael Souza <faelzsouza@gmail.com>
2021-10-14 17:51:10 -0300

    feat[app]: implement auth

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,10 +7,19 @@ import { ProfilesModule } from './profiles/profiles.module';
 import { UsersModule } from './users/users.module';
 import { PrismaService } from './prisma/prisma.service';
 import { AuthModule } from './auth/auth.module';
+import { APP_GUARD } from '@nestjs/core';
+import { JwtAuthGuard } from './auth/jwt-auth.guard';
 
 @Module({
   imports: [GamesModule, GenresModule, ProfilesModule, UsersModule, AuthModule],
   controllers: [AppController],
-  providers: [AppService, PrismaService],
+  providers: [
+    AppService,
+    PrismaService,
+    {
+      provide: APP_GUARD,
+      useClass: JwtAuthGuard,
+    },
+  ],
 })
 export class AppModule {}
