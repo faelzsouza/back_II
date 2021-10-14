486dfe2b9ff3e5f0ddc1b87bfa9940a2b3977451
Rafael Souza <faelzsouza@gmail.com>
2021-10-14 17:50:33 -0300

    chore[update-game-dto]: remove unused imports

--- src/games/dto/update-game.dto.ts
+++ src/games/dto/update-game.dto.ts
@@ -1,7 +1,4 @@
 import { PartialType } from '@nestjs/mapped-types';
-import { Type } from 'class-transformer';
-import { IsOptional, ValidateNested } from 'class-validator';
-import { ConnectProfileDto } from 'src/profiles/dto/connect-profile.dto';
 import { CreateGameDto } from './create-game.dto';
 
 export class UpdateGameDto extends PartialType(CreateGameDto) {}
